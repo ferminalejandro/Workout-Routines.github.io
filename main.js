267d7c4ca39e9431734e45bd099760bb546dff0d
ferminalejandro <fermintejadaa@gmail.com>
2025-01-15 16:40:49 -0800

    Update main.js

--- main.js
+++ main.js
@@ -11,4 +11,13 @@ toggleBtn.onclick = function () {
   toggleBtnIcon.classList.toggle('fa-xmark', isOpen);
 };
 
+const navLinks = document.querySelectorAll('.nav ul li a, .dropdown-menu ul li a');
+
+navLinks.forEach(link => {
+  link.addEventListener('click', (e) => {
+    e.preventDefault();
+    alert('Under Construction');
+  });
+});
+
 // Nav Bar Ends
